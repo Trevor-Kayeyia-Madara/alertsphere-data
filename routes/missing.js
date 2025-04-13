4c95f186bebe7f70526c115ee8805c504111e47c
Trevor-Kayeyia-Madara <trevor.madara@student.moringaschool.com>
2025-04-13 08:47:17 +0300

    Missing route

--- routes/missing.js
+++ routes/missing.js
@@ -1,9 +1,14 @@
 const express = require('express');
-const { reportMissingPerson } = require('../controllers/missingController');
+const { reportMissingPerson, updateMissingPersonStatus } = require('../controllers/missingController');
 
 const router = express.Router();
 
 module.exports = (supabase) => {
+  // Route to report a missing person
   router.post('/report', (req, res) => reportMissingPerson(req, res, supabase));
+
+  // Route to update missing person status (using report_id)
+  router.patch('/update/:reportId/status', (req, res) => updateMissingPersonStatus(req, res, supabase));
+
   return router;
 };
